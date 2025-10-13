cd0db7c0953e308ade63b057ca1032cf10fb9542
baudi2k <joseguancheb@gmail.com>
2025-10-13 16:48:36 +0100

    feat: Implement search input feature in App component and update dependencies

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input } from '@rneui/themed';
+import { Icon } from '@rneui/base';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <Input 
+          placeholder='Search apps & games'
+          leftIcon={
+            <Icon
+              name='search'
+              type='font-awesome'
+              size={20}
+              color='#86939e'
+            />
+          }
+          inputContainerStyle={styles.searchInput}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +27,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#121212',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    width: '70%',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 10,
+    marginTop: 50,
+  },
+  searchInput: {
+    borderRadius: 20,
+    borderWidth: 0,
+    borderBottomWidth: 0,
+    backgroundColor: '#2A2A2A',
+    paddingHorizontal: 10,
+  }
 });
